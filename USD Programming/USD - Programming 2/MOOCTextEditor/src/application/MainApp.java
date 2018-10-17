2cb94b4fc6e08528f7c10d42585f408a78fc6b37
cmogush <cmogush@gmail.com>
2018-10-17 06:46:56 -0400

    Add files via upload

--- USD Programming/USD - Programming 2/MOOCTextEditor/src/application/MainApp.java
+++ USD Programming/USD - Programming 2/MOOCTextEditor/src/application/MainApp.java
@@ -3,7 +3,6 @@ package application;
 import java.io.IOException;
 import java.util.List;
 
-import document.BasicDocument;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
